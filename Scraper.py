060d5e9ec7873633b7746d180e96bfaaa77e2b15
ransomware <mohamed.tahrioui@wheregroup.com>
2017-06-22 23:42:21 +0200

    Add generic isUserLink version

--- Scraper.py
+++ Scraper.py
@@ -3,3 +3,17 @@ class Scraper():
     def getUserLink(cls, username):
         return "/".join(["https://www.instagram.com", username] )+ "/"
 
+    @classmethod
+    def isUserLink(cls, address,title):
+
+        return address is not None and \
+               title in address and \
+               "/accounts/" not in address and \
+               "/p/" not in address and \
+               "/legal/" not in address and \
+               '/blog.instagram.com/' not in address and \
+               '/about/' not in address and \
+               '/explore/' not in address and \
+               '/developer/' not in address and \
+               'instagram-press.com' not in address and \
+               'help.instagram.com' not in address
