d37a2c2aa0c22197ef6849913b112cea1aa48358
ransomware <mohamed.tahrioui@wheregroup.com>
2017-06-24 14:34:48 +0200

    Add removeAllKeys,listToKeyDict methods to DictUtils

--- DictUtils.py
+++ DictUtils.py
@@ -1,2 +1,13 @@
 def listToDict(keyFunction, values):
     return dict((keyFunction(v), v) for v in values)
+
+
+def listToKeyDict(keyFunction, values):
+    return dict((keyFunction(v), {}) for v in values)
+
+
+
+def removeAllKeys(dictionary, keys):
+    for key in keys:
+        dictionary.pop(key, None)
+    return dictionary
